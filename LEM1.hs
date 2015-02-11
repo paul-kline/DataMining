7e7d0490758e556480e85ef198d046ce3a796661
Paul Ian Kline <PaulIanKline@gmail.com>
2015-02-10 23:47:27 -0600

    just more work yall

--- LEM1.hs
+++ LEM1.hs
@@ -1,7 +1,10 @@
+{-#LANGUAGE TypeSynonymInstances, FlexibleInstances, ViewPatterns #-}
 module LEM1 where
 
 import Prelude hiding (id)
 import Data.List
+import Control.Monad (join)
+columnName = ["Size", "Color", "Feel", "Temperature", "Attitude"]
 
 row1 = Row 1 "big" "yellow" "soft" "low" "positive"
 row2 = Row 2 "big" "yellow" "hard" "high" "negative"
@@ -13,6 +16,29 @@ row7 = Row 7 "big" "blue" "hard" "low" "so-so"
 row8 = Row 8 "big" "blue" "hard" "high" "so-so"
 
 dat = [row1,row2,row3,row4,row5,row6,row7,row8]
+partSize = mySort $ compPart dat [size]
+partColor = mySort $ compPart dat [color]
+partFeel = mySort $ compPart dat [feel]
+partTemp = mySort $ compPart dat [temp]
+
+partSize_Color = mySort $ compPart dat [size,color]
+partSize_Feel = mySort $ compPart dat [size,feel]
+partSize_Temp = mySort $ compPart dat [size,temp]
+partColor_Feel = mySort $ compPart dat [color,feel]
+partColor_Temp = mySort $ compPart dat [color,temp]
+partFeel_Temp = mySort $ compPart dat [feel,temp]
+
+partSize_Color_Feel = mySort $ compPart dat [size,color,feel]
+partSize_Color_Temp = mySort $ compPart dat [size,color,temp]
+partSize_Feel_Temp = mySort $ compPart dat [size,feel,temp]
+partColor_Feel_Temp = mySort $ compPart dat [color,feel,temp]
+
+aStar = mySort $ compPart dat [size,color,feel,temp]
+dStar = mySort $ compPart dat [att]
+
+partAttPlus = mySort $ decisionPart dat att "positive"
+partAttSoSo = mySort $ decisionPart dat att "so-so"
+partAttNegative = mySort $ decisionPart dat att "negative"
 
 data Row = Row { id    :: Int,
              size  :: String,
@@ -26,12 +52,15 @@ type Attribute = (Row -> String)
 type Decision  = (Row -> String)
 type Extractor = (Row -> String)
 
+  
+  
+{-
 getPart :: [Row] -> Extractor -> String -> [Int]
 getPart [] _ _ = []
 getPart (x:xs) f str = if (f x) == str then (id x) : (getPart xs f str)
                                        else getPart xs f str 
                                        
-
+-}
                                        
 decisionPart :: [Row] -> Extractor -> String -> [[Int]]
 decisionPart [] _ _ = []
@@ -97,5 +126,18 @@ isSubSet _ []  = False --is something a subset of nothing?
 isSubSet (x:xs) s = if x `elem` s  then isSubSet xs s
                                    else False
 
+data Rule = Rule [(Extractor,String)] (Extractor,String)
+ruleSize_Big_Color_Yellow_Feel_Hard = Rule [(size, "big"),(color,"yellow"),(feel,"hard")] (att, "negative")
 
-             
+instance Show Rule where
+ show (Rule pairLS pair) = "[ ==" ++ (join (intersperse ", ==" (map (snd) pairLS))) ++ "] ==> " ++ (snd pair)
+ 
+ruleCovers :: [Row] -> Rule -> [Int]
+ruleCovers table (Rule pairls decPair) = let extractors = map fst pairls in
+                                            let shoulbes = map snd pairls in
+                                               join (map (\row -> if (getVals extractors row ) == shoulbes then [(id row)]
+                                                                                        else []) table)
+ruleIsConsistent :: [Row] -> Rule -> Bool
+ruleIsConsistent table r@(Rule pairls pair) = let covers = ruleCovers table r in
+                                                       let betterBeInThisPart = head $ decisionPart table (fst pair) (snd pair) in
+                                                          and $ map ((flip elem) betterBeInThisPart) covers
